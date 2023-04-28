17ab5f5f69211ccf64e70d43cf40123f4331c96c
cryptondee <cryptondee@gmail.com>
2023-04-28 18:59:28 +0200

    reports, db

--- components/Test.jsx
+++ components/Test.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Alchemy, AlchemySubscription, Network } from "alchemy-sdk";
 import axios from "axios";
+import mongoose from "mongoose";
 
 function Test() {
   const [items, setItems] = useState([]);
@@ -8,6 +9,23 @@ function Test() {
   const [tokenB, setB] = useState();
   const [hash, setHash] = useState();
   const [report, setReport] = useState();
+
+  mongoose.connect(
+    "mongodb+srv://ndwong2012:lfQB9KI8vSRSp0OD@cluster0.fwdsenb.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+  const responseSchema = new mongoose.Schema({
+    url: { type: String, required: true },
+    tokenA: { type: String, required: true },
+    tokenB: { type: String, required: true },
+    report: { type: Object, required: true },
+  });
+
+  const Response = mongoose.model("Response", responseSchema);
 
   const config = {
     apiKey: process.env.alchemyAPI,
@@ -18,7 +36,8 @@ function Test() {
   const filter = {
     address: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
     topics: [
-      "0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9",
+      "0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9", // pairCreated
+      "0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c", // deposit
     ],
   };
   const weth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
@@ -44,17 +63,23 @@ function Test() {
 
   const getReport = async (tokenA, tokenB) => {
     if (tokenA === weth) {
+      const url = `${baseURL}${tokenB}`;
       const report = await axios
-        .get(`${baseURL}${tokenB}`)
+        .get(`${url}`)
         .then((res) => res.data)
         .then((data) => console.log(data.result));
       setReport(report);
+      const newResponse = new Response({ url, tokenA, tokenB, report });
+      await newResponse.save();
     } else {
+      const url = `${baseURL}${tokenA}`;
       const report = await axios
-        .get(`${baseURL}${tokenA}`)
+        .get(`${url}`)
         .then((res) => res.data)
         .then((data) => console.log(data.result));
       setReport(report);
+      const newResponse = new Response({ url, tokenA, tokenB, report });
+      await newResponse.save();
     }
   };
 
@@ -69,7 +94,7 @@ function Test() {
       <h2>hash</h2>
       {hash}
       <h2>Report</h2>
-      {report}
+      {JSON.stringify(report, null, 2)}
     </div>
   );
 }
