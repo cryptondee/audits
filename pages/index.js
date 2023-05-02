600ee338c73dd118f3e5b84cf3daa57cd042fe32
NDW <cryptondee@gmail.com>
2023-05-02 18:16:53 +0200

    henlo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Test from "@/components/Test.jsx";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Test />
+      henlo
     </main>
   );
 }
