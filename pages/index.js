3090171c0d08d58e9ea22c92d38d3fddeb4ce745
NDW <cryptondee@gmail.com>
2023-05-03 11:22:43 +0200

    frontend

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 import Test from "@/components/Test.jsx";
 import React from "react";
 import DisplayData from "@/components/DisplayData";
-export default function Home() {
-  return <div>henlo</div>;
+import clientPromise from "@/lib/dbConnect";
+
+export default function Home({ data }) {
+  return (
+    <main>
+      <div>
+        <h2>Token audits</h2>
+        <p>amount of tokens in the db: {data}</p>
+      </div>
+    </main>
+  );
+}
+export async function getServerSideProps() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("test");
+    const reports = await db.collection("test").countDocuments();
+    return {
+      props: { data: JSON.parse(JSON.stringify(reports)) },
+    };
+  } catch (err) {
+    console.error(err);
+  }
 }
